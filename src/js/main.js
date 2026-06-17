19b3bcde2dabc9dc00df10ce39841eb7ff33c581
Nathanael Kenzey <nathanael75kenzey@gmail.com>
2026-06-17 09:11:16 +0200

    init fetch file for index.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,64 @@
-const afficherCards = (async) => {
-    const response = fetch("data/spectacles.json");
-    const data = response.json();
-    console.log(data[0]);
+const cardsShows = async () => {
+    try {
+        // ? i get data ?
+        const response = await fetch("/src/data/spectacles.json");
+        // ? i convert them to use them in json format ?
+        const data = await response.json();
+        // ? Je recupere mon id "shows-grid" dans mon html ?
+        const grid = document.getElementById("Shows-grid");
+        data.spectacles.slice(0, 3).forEach((n) => {
+            const card = document.createElement("article");
+            card.className("card");
+
+            const affichage = document.createElement("span");
+            affichage.className = "card__visual";
+
+            const badge = document.createElement("span");
+            badge.className = "card__badge";
+            badge.textContent = n.type;
+
+            const body = document.createElement("div");
+            body.className = "card__body";
+
+            const title = document.createElement("h3");
+            title.className = "card__title";
+            title.textContent = n.titre;
+
+            const meta = document.createElement("div");
+            meta.className = "card__meta";
+
+            const artist = document.createElement("span");
+            artist.className = "card__artist";
+            artist.textContent = n.artiste;
+
+            const date = document.createElement("span");
+            date.className = "card__date";
+            date.textContent = n.date;
+
+            const horaire = document.createElement("span");
+            horaire.className = "card__hours";
+            horaire.textContent = n.horaire;
+
+            const btn = document.createElement("a");
+            btn.className = "card__btn";
+            btn.href = "pages/programmation.html";
+            btn.textContent = "Réserver";
+
+            meta.appendChild(artist);
+            meta.appendChild(date);
+            meta.appendChild(horaire);
+            visual.appendChild(badge);
+            body.appendChild(title);
+            body.appendChild(meta);
+            body.appendChild(btn);
+            card.appendChild(affichage);
+            card.appendChild(body);
+
+            grid.appendChild(card);
+        });
+    } catch (error) {
+        console.error("Erreur lors du chargement des données:", error);
+    }
 };
+
+cardsShows();
